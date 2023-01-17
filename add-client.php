fe04d9b0216bfc2badb660304d773651508a3d6a
Aditya JP <adityajai243@gmail.com>
2023-01-17 14:04:29 +0530

    Add files via upload

---
+++ add-client.php
@@ -0,0 +1,40 @@
+<?php include 'navbar.php'; ?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Infits - Add Client</title>
+        <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    </head>
+<style>
+    .heading p {
+        font-family: 'NATS';
+        font-style: normal;
+        font-weight: 400;
+        padding-left: 100px;
+        padding-top: 12px;
+        font-size: 44px;
+        line-height: 70px;
+        color: #000000;
+    }
+    
+    p { 
+        margin-top: 0px;
+        margin-bottom: 1rem;
+    }
+
+    
+    
+</style>
+<body>
+    <div class = "container">
+        <div class = "heading">
+            <p> Add a Client <p>
+        </div>
+        <div class = "requests">
+            <p> Send Requests </p>
+            <p> Pending Requests </p>
+        </div>
+    </div>
+</body>
+</html>
