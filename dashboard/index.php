43ec75d8d4eeaeea2ab6cabfc5e95acf2eb53a35
Aditya JP <adityajai243@gmail.com>
2023-03-16 09:55:03 +0530

    Create index.php

---
+++ dashboard/index.php
@@ -0,0 +1,75 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Client</title>
+    <script src="https://kit.fontawesome.com/78cfc0771c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+  </head>
+  <body>
+    <div class = "wrapper">
+        <div class="sidebar">
+            <h2><img src = "assets/infit-transparent.png"></h2>
+            <ul>
+            <li>
+                <a href="#"><img src="assets/Vector.png"> Dashboard</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-chat-left-dots"></i> Messages</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-broadcast"></i> Live</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-calendar3"></i></i> Appointments</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-people"></i> Clients</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-coin"></i> Payment</a>
+            </li>
+        </ul>
+        <ul class="social_media">
+            <li>
+                <a href="#"><i class="bi bi-exclamation-circle"></i> Get Help</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-gear"></i> Settings</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-box-arrow-right"></i> Logout</a>
+            </li>
+        </ul>    
+        </div>
+        <div class="main_content">
+            <div class="header">
+                Good Morning, <b>John Wayne</b>
+                <div class="small-font">Your perforance summary this week</div>
+            </div>
+            <div class = "client">
+                <h1>Client List</h1>
+                <div class="btn-group">
+                    <a href="#" class="active" aria-current="page">Active</a>
+                    <a href="#" class="pending">Pending</a>
+                  </div>
+                <div class = "client-list-top">
+                    <img src = "assets/lilac user icon.png">
+                </div>
+                <div class = "client-content">
+                    <h2>No Clients added yet</h2>
+                    <button type="button" class="client-button"> <h3> Send Request</h3></button>
+                </div>
+                <div class = "client-list-bottom">
+                    <img src = "assets/lilac user icon.png">
+                </div>
+
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
