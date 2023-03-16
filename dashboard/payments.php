de7bf45d15cf261a1611694273a9d9bf0c697e7c
Aditya JP <adityajai243@gmail.com>
2023-03-16 09:55:34 +0530

    Create payments.php

---
+++ dashboard/payments.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Billing</title>
+    <script src="https://kit.fontawesome.com/78cfc0771c.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" />
+  </head>
+  <body>
+    <div class = "wrapper">
+        <div class="sidebar">
+            <h2><img src = "assets/infit-transparent.png"></h2>
+            <ul>
+            <li>
+                <a href="#"><img src="assets/Vector.png"> Dashboard</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-chat-left-dots"></i> Messages</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-broadcast"></i> Live</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-calendar3"></i></i> Appointments</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-people"></i> Clients</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-coin"></i> Payment</a>
+            </li>
+        </ul>
+        <ul class="social_media">
+            <li>
+                <a href="#"><i class="bi bi-exclamation-circle"></i> Get Help</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-gear"></i> Settings</a>
+            </li>
+            <li>
+                <a href="#"><i class="bi bi-box-arrow-right"></i> Logout</a>
+            </li>
+        </ul>    
+        </div>
+        <div class="main_content">
+            <div class="header">
+                Good Morning, <b>John Wayne</b>
+                <div class="small-font">Your perforance summary this week</div>
+            </div>
+            <div class = "client">
+                <h1>Billing and Invoices</h1>
+        </div>
+        <div class = "billing">
+            <img src ="assets/cash.png">
+        </div>
+        <div class = "bill-content">
+            <h3>No Bills issued currently</h3>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+  </body>
+</html>
